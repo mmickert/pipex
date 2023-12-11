84aa7a05388559edab1e70b33221d3c7bb2fe0aa
mmickert <mickert@student-42heilbronn.de>
2023-12-11 18:04:54 +0100

    works, but not with wrong input. and not all mine

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mickert <mickert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:40:22 by mickert           #+#    #+#             */
-/*   Updated: 2023/12/10 15:58:44 by mickert          ###   ########.fr       */
+/*   Updated: 2023/12/11 17:18:38 by mickert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@
 int		main(int argc, char **argv, char **envp);
 void	parent_process(char **argv, int *p_fd, char **envp);
 void	child_process(char **argv, int *p_fd, char **envp);
-int		execute(char *argv, char **envp);
-char	*cmd_path(char *cmd, char **envp);
+void		execute(char *argv, char **envp, int *p_fd);
+char	*command_path(char *cmd, char **envp);
+int		fd_open(char *argv, int flag);
 
 #endif
--- source/pipex.c
+++ source/pipex.c
@@ -6,7 +6,7 @@
 /*   By: mickert <mickert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 15:39:26 by mickert           #+#    #+#             */
-/*   Updated: 2023/12/10 16:08:27 by mickert          ###   ########.fr       */
+/*   Updated: 2023/12/11 17:45:30 by mickert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,123 +22,120 @@ int	main(int argc, char **argv, char **envp)
 		ft_printf("Usage: <file1> <cmd1> <cmd2> <file2>\n");
 		exit(1);
 	}
-	else if (pipe(p_fd) == -1)/*pipe has 2 ends, p_fd[1] = writing && p_fd[0] = reading*/
+	else if (pipe(p_fd) == -1) /*pipe has 2 ends, p_fd[1] = writing && p_f[0] = reading*/
 	{
-		perror("Pipe creation failed\n");
+		perror("pipe");
 		exit(1);
 	}
 	pid = fork();/*fork returns 1. process ID of the cild in parent 2. 0 == child*/
 	if (pid == -1)
 	{
-		perror("Fork creation failed\n");
+		perror("fork");
 		exit(1);
 	}
-	else if (pid == 0)
+	if (pid == 0)
 		child_process(argv, p_fd, envp);
-	waitpid(pid, NULL, 0);
 	parent_process(argv, p_fd, envp);
+	waitpid(pid, NULL, 0); /*3. arg == wait for all children*/
 	return (0);
 }
 
-/// 
-int	execute_parent(char *argv, char **envp, int *p_fd)
+void	execute(char *argv, char **envp, int *p_fd)
 {
 	char	**cmd;
-	char	*cmdpath;
+	char	*cmd_path;
 	int		i;
-	int		fd;
 
+	(void)p_fd;
 	i = 0;
 	cmd = ft_split(argv, ' ');
 	if (access(cmd[0], X_OK) == 0)
-		cmdpath = cmd[0];
+		cmd_path = cmd[0];
 	else
-		cmdpath = cmd_path(cmd[0], envp);
-	fd = open(&argv[3], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	close(p_fd[1]);
-	dup2(fd, 1);
-	close(fd);
-	dup2(p_fd[0], 0);
-	close(p_fd[0]);
-	execve(cmdpath, cmd, envp);
-	while (cmd[i])
+		cmd_path = command_path(cmd[0], envp);
+	if (execve(cmd_path, cmd, envp) == -1)
 	{
-		free(cmd[i]);
-		i++;
+		ft_printf("failure execve");
+		exit (1);
 	}
-	free(cmd);
-	perror("Error in execve");
-	exit (1);
+	// while (cmd[i])
+	// {
+	// 	free(cmd[i]);
+	// 	i++;
+	// }
+	// free(cmd);
+	// exit(1);
 }
-///
 
 void	parent_process(char **argv, int *p_fd, char **envp)
 {
 	pid_t	pid_2;
 	int		status;
+	int		fd_write;
 
-	(void)p_fd;
 	status = 0;
 	pid_2 = fork();
 	if (pid_2 < 0)
 	{
-		perror("second fork");
+		perror("fork");
 		exit(1);
 	}
-	if (pid_2 == 0)
-	{
-		if (execute_parent(argv[3], envp, p_fd) == 1)
-			perror("Error: executing command\n");
-	}
+	fd_write = fd_open(argv[4], 0);
 	close(p_fd[1]);
-	waitpid(pid_2, &status, 0);
+	dup2(fd_write, STDOUT_FILENO);
+	close(fd_write); 
+	dup2(p_fd[0], STDIN_FILENO);
+	close(p_fd[0]);
+	if (pid_2 == 0)
+		execute(argv[3], envp, p_fd);
+	close(p_fd[1]);
+	waitpid(pid_2, 0, 0);
 	status = WEXITSTATUS(status);
 }
 
-int	execute_child(char *argv, char **envp)
+int	fd_open(char *argv, int flag)
 {
-	char	**cmd;
-	char	*cmdpath;
-	int		i;
+	int	fd;
 
-	i = 0;
-	cmd = ft_split(argv, ' ');
-	if (access(cmd[0], X_OK) == 0)
-		cmdpath = cmd[0];
+	if (flag == 1)
+		fd = open(argv, O_RDONLY, 0777);
 	else
-		cmdpath = cmd_path(cmd[0], envp);
-	execve(cmdpath, cmd, envp);
-	while (cmd[i])
 	{
-		free(cmd[i]);
-		i++;
+		fd = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	} /*-> make an extra open function with error handling */
+	if (fd == -1)
+	{
+		if (errno == EACCES)
+			ft_putstr_fd("zsh: permission denied: ", 2);
+		else if (errno == ENOENT)
+			ft_putstr_fd("zsh: no such file or directory: ", 2);
+		else
+			ft_putstr_fd("zsh: error opening the file: ", 2);
+		ft_putstr_fd(argv, 2);
+		ft_putstr_fd("\n", 2);
 	}
-	free(cmd);
-	perror("Error in execve");
-	exit (1);
+	return (fd);
 }
 
 void	child_process(char **argv, int *p_fd, char **envp)
 {
 	int	fd_in;
+	int	flag;
 
-	fd_in = open(argv[1], O_RDONLY, 0777);
+	flag = 1;
+	fd_in = fd_open(argv[1], flag);
 	if (fd_in == -1)
-	{
-		perror("Error: Opening input file\n");
-		exit(1);
-	}
+		exit (1);
 	close(p_fd[0]);
+	dup2(fd_in, STDIN_FILENO);
+	close(fd_in);
 	dup2(p_fd[1], STDOUT_FILENO);
 	close(p_fd[1]);
-	dup2(fd_in, STDIN_FILENO);
-	close(*p_fd);
-	if (execute_child(argv[2], envp) == 1)
-		perror("Error: executing command\n");
+	execute(argv[2], envp, NULL);
 	exit(1);
 }
 
-///////////////
+//
 
 void	free_arrayofstrings(char **a)
 {
@@ -159,19 +156,20 @@ char	**get_path(char **envp)
 	int		i;
 
 	i = 0;
-	while (ft_strnstr(envp[i], "PATH", 4) == 0)
+	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
 		i++;
 	paths = ft_split(envp[i] + 5, ':');
 	return (paths);
 }
 
-char	*cmd_path(char *cmd, char **envp)
+char	*command_path(char *cmd, char **envp)
 {
 	char	*temp;
 	char	**paths;
 	int		i;
 	char	*path;
 
+	printf("hell\n");
 	paths = get_path(envp);
 	i = 0;
 	while (paths[i])
@@ -187,6 +185,7 @@ char	*cmd_path(char *cmd, char **envp)
 		free(temp);
 		free(path);
 	}
+	printf("hell3\n");
 	ft_putstr_fd("zsh: command not found: ", 2);
 	ft_putstr_fd(cmd, 2);
 	ft_putstr_fd("\n", 2);
